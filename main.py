fd845468fa032311f3164d8c23d666fde60dcd79
Alexey322 <youtube-up@mail.ru>
2025-02-12 00:33:56 +0300

    Bug fixes

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     asr_model = ASR()
 
     split_video_by_audio_chunks(asr_model=asr_model,
-                                video_path=f"IELTS Speaking Topics and Sample Answers.mp4.wav",
+                                video_path=r"C:\Users\Alexey\Desktop/example.wav",
                                 save_dir='chunks',
                                 min_chunk_duration=3,
                                 max_chunk_duration=5,
-                                min_time_between_words_for_separation=0.1,
-                                min_chunk_word_score=0.5)
+                                min_time_between_words_for_separation=0.2,
+                                min_chunk_word_score=0.5,
+                                extra_time_for_chunk_borders=0.1)
     
